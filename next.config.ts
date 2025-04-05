004692b72d221e5bb1081d03a89e198ad5a5d8c8
Ifthikar20 <ifthicaralikhan@gmail.com>
2025-04-04 21:58:06 -0500

    removed experimental next config

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,16 @@
 const nextConfig = {
   images: {
     domains: [
-      "media.giphy.com", 
+      "media.giphy.com",
       "media0.giphy.com",
-      "media1.giphy.com", 
+      "media1.giphy.com",
       "media2.giphy.com",
       "media3.giphy.com",
       "media4.giphy.com",
       "i.pravatar.cc"
     ], // ✅ Add all Giphy subdomains as allowed domains
   },
-  experimental: {
-    serverComponentsExternalPackages: ['zlib'],
-  },
+  serverExternalPackages: ['zlib'],
 };
 
 module.exports = nextConfig;
